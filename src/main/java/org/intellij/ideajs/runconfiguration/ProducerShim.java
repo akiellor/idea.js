9a1205f04371b20a22a6afea356fd25742680d2c
Andrew Kiellor <akiellor@gmail.com>
2013-09-17 19:15:21 -0700

    Log errors.

--- src/main/java/org/intellij/ideajs/runconfiguration/ProducerShim.java
+++ src/main/java/org/intellij/ideajs/runconfiguration/ProducerShim.java
@@ -7,6 +7,7 @@ import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.application.ApplicationConfiguration;
 import com.intellij.execution.application.ApplicationConfigurationType;
 import com.intellij.execution.junit.RuntimeConfigurationProducer;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
@@ -16,6 +17,9 @@ import com.intellij.psi.util.ClassUtil;
 import org.jetbrains.annotations.Nullable;
 
 public class ProducerShim extends RuntimeConfigurationProducer implements Cloneable {
+
+    private static Logger log = Logger.getInstance(ProducerShim.class);
+
     @SuppressWarnings("unused")
     private PsiFile containingFile;
 
@@ -31,26 +35,35 @@ public class ProducerShim extends RuntimeConfigurationProducer implements Clonea
     @Nullable
     @Override
     protected RunnerAndConfigurationSettings createConfigurationByElement(Location location, final ConfigurationContext context) {
-        Module module = context.getModule();
-        if (module == null) { return null; }
+        try {
+            Module module = context.getModule();
+            if (module == null) {
+                return null;
+            }
 
-        JavascriptRuntime runtime = new JavascriptRuntime(context.getProject());
-        ScriptedProducer scriptedProducer = new ScriptedProducer(runtime);
+            JavascriptRuntime runtime = new JavascriptRuntime(context.getProject());
+            ScriptedProducer scriptedProducer = new ScriptedProducer(runtime);
 
-        Optional<? extends Configuration> configuration = scriptedProducer.produce(context);
-        if(!configuration.isPresent()) {
+            Optional<? extends Configuration> configuration = scriptedProducer.produce(context);
+            if (!configuration.isPresent()) {
+                return null;
+            }
+
+            PsiClass mainClass = ClassUtil.findPsiClass(PsiManager.getInstance(context.getProject()), configuration.get().main());
+            if (mainClass == null) {
+                return null;
+            }
+
+            RunnerAndConfigurationSettings settings = cloneTemplateConfiguration(context.getProject(), context);
+            ApplicationConfiguration intellijConfiguration = (ApplicationConfiguration) settings.getConfiguration();
+            intellijConfiguration.setMainClass(mainClass);
+            intellijConfiguration.setProgramParameters(configuration.get().arguments());
+            intellijConfiguration.setName(configuration.get().name());
+            return settings;
+        } catch (Exception e) {
+            log.error(e);
             return null;
         }
-
-        PsiClass mainClass = ClassUtil.findPsiClass(PsiManager.getInstance(context.getProject()), configuration.get().main());
-        if(mainClass == null) { return null; }
-
-        RunnerAndConfigurationSettings settings = cloneTemplateConfiguration(context.getProject(), context);
-        ApplicationConfiguration intellijConfiguration = (ApplicationConfiguration) settings.getConfiguration();
-        intellijConfiguration.setMainClass(mainClass);
-        intellijConfiguration.setProgramParameters(configuration.get().arguments());
-        intellijConfiguration.setName(configuration.get().name());
-        return settings;
 
     }
 
